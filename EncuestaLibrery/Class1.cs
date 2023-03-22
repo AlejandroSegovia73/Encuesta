59dd24496fb530575c051519182991a00cf3f03f
Alejandro Segovia <fcblade676@gmail.com>
2023-03-22 10:02:53 -0600

    Primera version

---
+++ EncuestaLibrery/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncuestaLibrery
+{
+    public class Class1
+    {
+        EncuestaTareaEntities db = new EncuestaTareaEntities();
+
+        public List<Encuesta> Listar()
+        {
+            var encuesta = from a in db.Encuesta
+                           select a;
+            return encuesta.ToList();
+        }
+
+
+        public void Agregar(Encuesta itm)
+        {
+            db.Encuesta.Add(itm);
+            db.SaveChanges();
+        }
+
+        public EncuestaLibrery.Encuesta Buscar(int id)
+        {
+            var encuesta = from a in db.Encuesta
+                           where a.id == id
+                           select a;
+
+            return encuesta.FirstOrDefault();
+        }
+
+        public void Editar(EncuestaLibrery.Encuesta item)
+        {
+            var encuesta = (from a in db.Encuesta
+                            where a.id == item.id
+                            select a).FirstOrDefault();
+
+            encuesta.equipo = item.equipo;
+            encuesta.nombreArea = item.nombreArea;
+           
+
+            db.SaveChanges();
+        }
+        public void Eliminar(EncuestaLibrery.Encuesta item)
+        {
+            var encuesta = (from a in db.Encuesta
+                            where a.id == item.id
+                            select a).FirstOrDefault();
+
+            db.Encuesta.Remove(encuesta);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Evaluacion/Controllers/EncuestaController.cs
@@ -0,0 +1,62 @@
+﻿using Evaluacion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Evaluacion.Controllers
+{
+    public class EncuestaController : Controller
+    {
+        EncuestaLibrery.Class1 a = new EncuestaLibrery.Class1(); 
+        // GET: Encuesta
+        public ActionResult Index()
+        {
+            var encuesta = a.Listar();
+         
+            return View(encuesta);
+        }
+
+
+
+        public ActionResult Create()
+        {
+            return View(new EncuestaLibrery.Encuesta ());
+        }
+        [HttpPost]
+        public ActionResult Create(EncuestaLibrery.Encuesta itm)
+        {
+            a.Agregar(itm);
+            return View("Index", a.Listar());
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(a.Buscar(id));
+
+        }
+
+        [HttpPost]
+
+        public ActionResult Edit(EncuestaLibrery.Encuesta item)
+        {
+            a.Editar(item);
+            return View("Index", a.Listar());
+
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            return View(a.Buscar(Id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(EncuestaLibrery.Encuesta item)
+        {
+            a.Eliminar(item);
+            return View("index", a.Listar());
+        }
+
+    }
+}
